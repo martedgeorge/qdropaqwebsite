595a5613a07b77f0d8363c3166dff7a02dc5b454
martedgeorge <martedgeorge@gmail.com>
2026-06-08 18:45:27 -0400

    Add new routes for Services, Staff, Library, and FeesForms

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,18 +11,29 @@ import Contact from './pages/Contact.jsx';
 import WhoWeServe from './pages/WhoWeServe.jsx';
 import Resources from './Resources.jsx';
 
+import Services from './pages/Services.jsx';
+import Staff from './pages/Staff.jsx';
+import Library from './pages/Library.jsx';
+import FeesForms from './pages/FeesForms.jsx';
+
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/who-we-are" element={<WhoWeAre />} />
+        <Route path="/overview" element={<WhoWeAre />} />
         <Route path="/why-qdropaq" element={<WhyQdropaq />} />
         <Route path="/process" element={<Process />} />
         <Route path="/faq" element={<FAQ />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/who-we-serve" element={<WhoWeServe />} />
         <Route path="/resources" element={<Resources />} />
+
+        <Route path="/services" element={<Services />} />
+        <Route path="/staff" element={<Staff />} />
+        <Route path="/library" element={<Library />} />
+        <Route path="/fees-forms" element={<FeesForms />} />
       </Route>
     </Routes>
   );
