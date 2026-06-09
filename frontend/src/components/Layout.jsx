05ddbcd685796faaeb35379a98d4782e5a5e81c7
martedgeorge <martedgeorge@gmail.com>
2026-06-08 22:47:51 -0400

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -10,7 +10,7 @@ export default function Layout() {
         </Link>
 
         <nav className="siteNav">
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/">Hoe</NavLink>
 
           <NavLink to="/overview">
             Overview
